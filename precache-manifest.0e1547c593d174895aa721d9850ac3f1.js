3d168ad6b2fdf43dde1d179702357ade283d2e9e
mellisdesigns <mellisdesigns@gmail.com>
2019-01-05 17:34:10 +1100

    Updates

---
+++ precache-manifest.0e1547c593d174895aa721d9850ac3f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cbe7968373643f41843",
+    "url": "/applauser/static/js/runtime~main.0cbe7968.js"
+  },
+  {
+    "revision": "f9b193bb982e3a2887bb",
+    "url": "/applauser/static/js/main.f9b193bb.chunk.js"
+  },
+  {
+    "revision": "0362e2533c44b9d0e523",
+    "url": "/applauser/static/js/1.0362e253.chunk.js"
+  },
+  {
+    "revision": "f9b193bb982e3a2887bb",
+    "url": "/applauser/static/css/main.abaab8dd.chunk.css"
+  },
+  {
+    "revision": "f6fdec2b1a91b4fa9916cec9b898fd06",
+    "url": "/applauser/index.html"
+  }
+];
